f61fd3a3e6f2aba2d8687b86a8708b21f275150a
Jacob Dahleen <jacob.dahleen@gmail.com>
2016-12-03 16:23:18 -0600

    Complete part 1 of day 1

--- 2016/Day01_TaxiCabDirections.py
+++ 2016/Day01_TaxiCabDirections.py
@@ -1,6 +1,7 @@
-from enum import Enum
+from enum import IntEnum
+import argparse
 
-class Direction(Enum):
+class Direction(IntEnum):
 	North = 0
 	East = 1
 	South = 2
@@ -9,3 +10,28 @@ class Direction(Enum):
 
 print('Get Directions!')
 
+currentDirection = Direction.North
+
+distances = [0] * 4
+# print(distances)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--turns', type=str, nargs='+')
+args = parser.parse_args()
+
+for turn in args.turns:
+	turn = turn.replace(',','')
+	# print(turn)
+	turnDirection = turn[0]
+	distance = int(turn[1:])
+	if turnDirection == 'L':
+		currentDirection = (currentDirection - 1) % Direction.MaxDirections
+	else:
+		currentDirection = (currentDirection + 1) % Direction.MaxDirections
+	distances[currentDirection] += distance
+
+print(distances)
+
+totalDistance = abs(distances[Direction.North] - distances[Direction.South]) + abs(distances[Direction.East] - distances[Direction.West])
+
+print(totalDistance)
