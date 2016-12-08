95c687fcfa2d0adfd91ca8856de8706afe45e6b4
Jacob Dahleen <jacob.dahleen@gmail.com>
2016-12-07 21:05:11 -0600

    Complete Day 1 Part 2

--- 2016/Day01_TaxiCabDoubleVisit.py
+++ 2016/Day01_TaxiCabDoubleVisit.py
@@ -16,9 +16,10 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--turns', type=str, nargs='+')
 args = parser.parse_args()
 
-pointsVisited = {}
+pointsVisited = {(0,0):1}
 currentNS = 0
 currentEW = 0
+foundIt = False
 
 for turn in args.turns:
 	turn = turn.replace(',','')
@@ -29,7 +30,31 @@ for turn in args.turns:
 		currentDirection = (currentDirection - 1) % Direction.MaxDirections
 	else:
 		currentDirection = (currentDirection + 1) % Direction.MaxDirections
-	for x in range(1,distance):
+	for x in range(0,distance):
+		# Add/Subtract 1 in direction of travel
+		if currentDirection == Direction.North:
+			currentNS += 1
+			# print('Going North')
+		elif currentDirection == Direction.South:
+			currentNS -= 1
+			# print('Going South')
+		elif currentDirection == Direction.East:
+			currentEW += 1
+			# print('Going East')
+		elif currentDirection == Direction.West:
+			currentEW -= 1
+			# print('Going West')
 		# check if tuple in pointsVisited
-		# if yes break out and calculate distance from tuple
+		if (currentEW,currentNS) in pointsVisited:
+			print("Found it at E/W:" + str(currentEW) + " N/S:" + str(currentNS))
+			# if yes break out and calculate distance from tuple
+			foundIt = True
+			break
 		# if no add tuple to pointsVisited
+		else:
+			pointsVisited[currentEW,currentNS] = 1
+	if foundIt:
+		break
+
+print('Total Distance to HQ: ' + str(abs(currentNS) + abs(currentEW)))
+# print(pointsVisited)
